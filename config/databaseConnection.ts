7cd4915cbc9863464e59b34e37a1c6f9936f6502
Alessandro Gelmi <ale.gelmi2@gmail.com>
2022-07-21 12:53:43 +0200

    fix db connection

--- config/databaseConnection.ts
+++ config/databaseConnection.ts
@@ -1,10 +1,8 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan);
   } catch (err: any) {
     console.error(`${err.message}`);
